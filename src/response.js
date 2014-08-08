358a71cbd3699dbc51ee36f6d714fca1a6f45207
Jiri Vrany <jiri.vrany@tul.cz>
2014-08-08 10:48:58 +0200

    refactoring of response.js - get rid of if else hell

--- src/response.js
+++ src/response.js
@@ -5,59 +5,90 @@ define(function() {
 
     var checkOpCode = function checkOpCode(op_code, rec_view, buf_pos) {
 
-        var length, feature, value;
+        var length, feature, feature_val, op_codes;
 
-        /* OP CODES
-        * 3 CHANGE_L
-        * 4 CHANGE_R
-        * 5 CONFIRM_L
-        * 6 CONFIRM_R
-        * 7 USER_AUTH_REQUEST
-        * 8 USER_AUTH_FAILURE
-        * 9 USER_AUTH_SUCCESS
-        */
+
+        op_codes = {
+            3: 'CHANGE_L',
+            4: 'CHANGE_R',
+            5: 'CONFIRM_L',
+            6: 'CONFIRM_R',
+            7: 'USER_AUTH_REQUEST',
+            8: 'USER_AUTH_FAILURE',
+            9: 'USER_AUTH_SUCCESS'
+        };
 
         if (op_code === 8) { /* Is it command usr_auth_fail */
             var method = rec_view.getUint8(buf_pos + 1);
             if (method === 2) { /* Password method */
-                return {CMD: 'auth_passwd'};
+                return {
+                    CMD: 'auth_passwd'
+                };
             }
 
         } else if (op_code === 9) { /*user authorized*/
             var user_id = rec_view.getUint16(buf_pos + 1);
             var avatar = rec_view.getUint32(buf_pos + 3);
-            return {CMD: 'auth_succ', USER_ID: user_id, AVATAR_ID: avatar};
-            
-        } else if (op_code === 4) {
-            /* Change_R command */
+            return {
+                CMD: 'auth_succ',
+                USER_ID: user_id,
+                AVATAR_ID: avatar
+            };
+
+        } else if (op_code < 7) {
             length = rec_view.getUint8(buf_pos);
             feature = rec_view.getUint8(buf_pos + 1);
-        
-            if (feature === 4) { /* got token */
-                value = parseStringValue(rec_view, length, buf_pos);
-                return {CMD: 'CHANGE_R', FEATURE:'TOKEN', VALUE: value};
-            }
-            
-            return {CHANGE_R: feature};
-        } else if (op_code === 3) {
-            /* Change_L command */
-            length = rec_view.getUint8(buf_pos);
-            feature = rec_view.getUint8(buf_pos + 1);
-            if (feature === 5) { /* got DED */
-                value = parseStringValue(rec_view, length, buf_pos);
-                return {CMD: 'CHANGE_L', FEATURE:'DED', VALUE: value};
-            }
-            return {CHANGE_L:  feature};
-        } else if (op_code === 5) {
-            /* CONFIRM_L command */
+            feature_val = parseFeature(feature, rec_view, buf_pos, length);
+            return {
+                CMD: op_codes[op_code],
+                FEATURE: feature_val.FEATURE,
+                VALUE: feature_val.VALUE
+            };
         }
 
-        return {OP_CODE: op_code};
     };
 
-    var  parseStringValue = function parseStringValue(receivedDataView, length, buf_pos) {
+    /*
+     * Parses received ArrayBuffer, find correct feature name and value
+     * @param feature - int feature number
+     * @param  rec_view - DataView for received buffer
+     * @param buf_pos - int current reading posititon
+     * @param lenght - lenght of command
+     */
+    var parseFeature = function parseFeature(feature, rec_view, buf_pos, length) {
+        var value;
+        var string_features = {
+            3: 'HOST_URL',
+            4: 'TOKEN',
+            5: 'DED',
+            9: 'CLIENT_NAME',
+            10: 'CLIENT_VERSION'
+        };
+        
+        if (feature in string_features) { /* got token */
+            value = parseStringValue(rec_view, length, buf_pos);
+            return {
+                FEATURE:  string_features[feature],
+                VALUE: value
+            };
+        } else {
+            return {
+                FEATURE: feature,
+                VALUE: 'TBD'
+            };
+        }
+    };
+
+    /*
+     * Parses received ArrayBuffer returns stored string value
+     * @param  receivedDataView - DataView for received buffer
+     * @param buf_pos - int current reading posititon
+     * @param lenght - lenght of command
+     */
+
+    var parseStringValue = function parseStringValue(receivedDataView, length, buf_pos) {
         var i, result = '';
-        for (i = 0; i <= length-4; i++) {
+        for (i = 0; i <= length - 4; i++) {
             result += String.fromCharCode(receivedDataView.getUint8(buf_pos + 2 + i));
         }
         return result.slice(1);
@@ -85,7 +116,7 @@ define(function() {
             var op_code, cmd_len, result;
             var rec_view = new DataView(buffer);
             var buf_pos = 2;
-            
+
             var message_len = rec_view.getUint16(buf_pos);
             buf_pos += 2;
 
