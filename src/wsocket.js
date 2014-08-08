08722a3639eb5bd77a4c48f8bde821655cdaea90
Jiri Hnidek <jiri.hnidek@tul.cz>
2014-08-08 09:36:50 +0200

    Refactoring of code.

--- src/wsocket.js
+++ src/wsocket.js
@@ -20,24 +20,15 @@ define(['request', 'response', 'negotiation'], function(request, response, negot
             my_webscoket = new WebSocket(config.uri, config.version);
             my_webscoket.binaryType = 'arraybuffer';
 
-            my_webscoket.addEventListener('open', function(e) {
-                wsocket.onConnect(e, config.username);
-            });
             my_webscoket.addEventListener('error', wsocket.onError);
             my_webscoket.addEventListener('close', wsocket.onClose);
-            my_webscoket.addEventListener('message', function(e) {
-                wsocket.onMessage(e, config.username, config.passwd);
+
+            my_webscoket.addEventListener('open', function(evnt) {
+                wsocket.onConnect(evnt, config);
             });
-
-        },
-
-
-
-        onConnect: function onConnect(event, username) {
-            var buf;
-            console.log('[Connected] ' + event.code);
-            buf = request.handshake(username);
-            my_webscoket.send(buf);
+            my_webscoket.addEventListener('message', function(msg) {
+                wsocket.onMessage(msg, config);
+            });
         },
 
         onError: function onError(event) {
@@ -48,47 +39,52 @@ define(['request', 'response', 'negotiation'], function(request, response, negot
             console.log('[Disconnected], Code:' + event.code + ', Reason: ' + event.reason);
         },
 
-        onMessage: function onMessage(message, username, passwd) {
-            var buf, response_data;
+        onConnect: function onConnect(event, config) {
+            var buf;
+            console.log('[Connected] ' + event.code);
+            buf = request.handshake(config.username);
+            my_webscoket.send(buf);
+        },
+
+        onMessage: function onMessage(message, config) {
+            var response_data;
+
             if (message.data instanceof ArrayBuffer) {
                 if (!response.checkHeader(message.data)) {
-                    /*bad protocl version - close conection*/
                     my_webscoket.close();
                     return;
                 }
 
                 response_data = response.parse(message.data);
                 console.info(response_data);
+
                 response_data.forEach(function(cmd) {
                     if (cmd.CMD === 'auth_passwd') {
-                        buf = request.userAuth(username, passwd);
-                        /* Send the blob */
-                        my_webscoket.send(buf);
+                        wsocket.userAuthData(config)
                     } else if (cmd.CMD === 'auth_succ')  {
                         wsocket.confirmHost(response_data);
                     }
                 });
-
-
             }
         },
 
+        userAuthData: function userAuthData(config) {
+            var buf = request.userAuth(config.username, config.passwd);
+            /* Send the blob */
+            my_webscoket.send(buf);
+        },
+
         confirmHost: function confirmHost(response_data) {
+            var buf = negotiation.url(negotiation.CHANGE_R, my_webscoket.url);
 
-            var paket = negotiation.url(negotiation.CHANGE_R, my_webscoket.url);
-
-            paket = request.buffer_push(paket, negotiation.token(negotiation.CONFIRM_R, response_data[1].VALUE));
-            paket = request.buffer_push(paket, negotiation.token(negotiation.CHANGE_R, '^DD31*$cZ6#t'));
-            paket = request.buffer_push(paket, negotiation.ded(negotiation.CONFIRM_L, response_data[2].VALUE));
+            buf = request.buffer_push(buf, negotiation.token(negotiation.CONFIRM_R, response_data[1].VALUE));
+            buf = request.buffer_push(buf, negotiation.token(negotiation.CHANGE_R, '^DD31*$cZ6#t'));
+            buf = request.buffer_push(buf, negotiation.ded(negotiation.CONFIRM_L, response_data[2].VALUE));
             
-            paket = request.message(paket);
-            
-            my_webscoket.send(paket);
-            
-            
+            buf = request.message(buf);
+            /* Send the blob */
+            my_webscoket.send(buf);
         }
-
-
     };
 
     return wsocket;
