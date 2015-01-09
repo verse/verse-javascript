da0f1673c1256c75d3a69c86c0ff68b1d11c6307
Jiri Hnidek <jiri.hnidek@tul.cz>
2015-01-09 21:27:20 +0100

    Modified example to use new function of API createNode(...).

--- example/app_standalone.js
+++ example/app_standalone.js
@@ -3,43 +3,42 @@
 (function(console, verse, config) {
     'use strict';
 
-    var settings, nodeHandler, tagHandler, tagGroupHandler, layerHandler;
+    var settings, nodeHandler, tagHandler, tagGroupHandler, layerHandler, userID, avatarID;
 
 
     nodeHandler = function nodeHandler(data) {
         data.forEach(function(cmd) {
+            console.log(cmd);
             if (cmd.CMD === 'NODE_CREATE') {
                 verse.subscribeNode(cmd.NODE_ID);
                 console.log('subscribed node ' + cmd.NODE_ID);
-            } else {
-                console.log(cmd);
+                // Test creating new node, when node of avatar was created by server
+                if (cmd.NODE_ID === avatarID) {
+                    verse.createNode(userID, avatarID, 2000);
+                    console.log('created node with custom_type: 2000');
+                }
             }
         });
-
     };
 
     tagGroupHandler = function tagGroupHandler(data) {
         data.forEach(function(cmd) {
+            console.log(cmd);
             if (cmd.CMD === 'TAG_GROUP_CREATE') {
                 verse.subscribeTagGroup(cmd.NODE_ID, cmd.TAG_GROUP_ID);
                 console.info('subscribed tagGroup nodeId =' + cmd.NODE_ID + ' tagGroupId = ' + cmd.TAG_GROUP_ID);
-            } else {
-                console.log(cmd);
             }
         });
-
     };
 
     layerHandler = function layerHandler(data) {
         data.forEach(function(cmd) {
+            console.log(cmd);
             if (cmd.CMD === 'LAYER_CREATE') {
                 verse.subscribeLayer(cmd.NODE_ID, cmd.LAYER_ID);
                 console.info('subscribed Layer nodeId =' + cmd.NODE_ID + ' layerId = ' + cmd.LAYER_ID);
-            } else {
-                console.log(cmd);
             }
         });
-
     };
 
     tagHandler = function tagHandler(data) {
@@ -75,6 +74,8 @@
              */
             console.info('User ID: ' + userInfo.USER_ID);
             console.info('Avatar ID: ' + userInfo.AVATAR_ID);
+            userID = userInfo.USER_ID;
+            avatarID = userInfo.AVATAR_ID;
         },
         errorCallback: function(error) {
             /*
